58a6b10a0ad7a2f615170b72596b12c066be8097
kennedy <kenneyrit@gmail.com>
2023-02-13 05:48:28 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre
 
+  def initialize(title)
+      @title=title
+
+  end
+  def turn_page
+      p "Flipping the page...wow, you read fast!"
+  end
+
+  end 
 
